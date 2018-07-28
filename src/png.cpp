5309d27259bf8522733cca3c08ca2eb2b31fe724
Kugayama Nana <nonamea774@gmail.com>
2018-07-29 02:56:21 +0900

    impl colorType true color

--- src/png.cpp
+++ src/png.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <fstream>
 #include <vector>
+#include <cmath>
 #include <cstdint>
 #include <cctype>
 #include <cassert>
@@ -15,13 +16,14 @@ using std::end;
 namespace PNG {
   class PNG : public Image {
   public:
-    PNG(size_t width, size_t height) : _width{width}, _height{height} {}
+    PNG(size_t width, size_t height, std::vector<Pixel> pixels) : _width{width}, _height{height}, _pixels{pixels} {}
     size_t width() { return _width; }
     size_t height() { return _height; }
-    std::vector<Pixel> const& pixels() { }
+    std::vector<Pixel> const& pixels() { return _pixels; }
   private:
     size_t const _width;
     size_t const _height;
+    std::vector<Pixel> _pixels;
   };
 
   class Chunk {
@@ -174,7 +176,8 @@ namespace PNG {
     } else if(type == "IEND") {
       return readIEND(fs);
     }
-    assert(!"never come");
+    std::cerr << type << std::endl;
+    assert(!"unknown chunk");
   }
 
   std::vector<std::unique_ptr<Chunk>> readChunks(std::istream& fs) {
@@ -184,10 +187,147 @@ namespace PNG {
     do {
       c = readChunk(fs);
       type = c->type();
-      std::cout << type << std::endl;
+      std::cerr << type << std::endl;
       v.push_back(std::move(c));
     } while(type != "IEND");
     return v;
+  }
+
+  std::vector<Byte> concatIDAT(std::vector<std::unique_ptr<Chunk>>& cs) {
+    std::vector<Byte> v;
+    for(auto& c : cs) {
+      if(c->type() == "IDAT") {
+        auto ic = dynamic_unique_cast<IDATChunk>(std::move(c));
+        auto d = ic->data();
+        v.insert(end(v), begin(d), end(d));
+        c = std::make_unique<Chunk>("IDAT");
+      }
+    }
+    return v;
+  }
+
+  std::vector<Pixel> render(std::unique_ptr<IHDRChunk> ihdr, std::vector<Byte>& data) {
+    size_t const width = ihdr->width();
+    size_t const height = ihdr->height();
+    int const depth = ihdr->depth();
+    int const colorType = ihdr->colorType();
+    int const filter = ihdr->filter();
+    std::vector<Pixel> pixels(width * height);
+    for(size_t h{0}; h < height; ++h) {
+      for(size_t w{0}; w < width; ++w) {
+        int i = h * width + w;
+        Pixel p;
+        switch(colorType) {
+        case 0: // grayscale
+        {
+          switch(filter) {
+          case 0: // None
+            if(depth == 1) {
+              p.r = p.g = p.b = data[h * (width + 1) + w + 1] ? 255 : 0;
+            } else {
+              p.r = p.g = p.b = data[h * (width + 1) + w + 1];
+            }
+          }
+          break;
+        }
+        case 2: // color
+        {
+          auto add = [](Byte a, Byte b) { return Byte((static_cast<int>(a) + static_cast<signed char>(b) + 256) % 256); };
+          switch(data[h * (width * 3 + 1)]) { // filtertype
+          case 0: // None
+          {
+            int base = h * (width * 3 + 1) + w * 3 + 1;
+            p.r = data[base];
+            p.g = data[base + 1];
+            p.b = data[base + 2];
+            break;
+          }
+          case 1:
+          {
+            int base = h * (width * 3 + 1) + w * 3 + 1;
+            if(w == 0) {
+              p.r = data[base];
+              p.g = data[base + 1];
+              p.b = data[base + 2];
+            } else {
+              Pixel b = pixels[i - 1];
+              p.r = add(b.r, data[base]);
+              p.g = add(b.g, data[base + 1]);
+              p.b = add(b.b, data[base + 2]);
+            }
+            break;
+          }
+          case 2:
+          {
+            int base = h * (width * 3 + 1) + w * 3 + 1;
+            if(h == 0) {
+              p.r = data[base];
+              p.g = data[base + 1];
+              p.b = data[base + 2];
+            } else {
+              Pixel b = pixels[i - width];
+              p.r = add(b.r, data[base]);
+              p.g = add(b.g, data[base + 1]);
+              p.b = add(b.b, data[base + 2]);
+            }
+            break;
+          }
+          case 3:
+          {
+            int base = h * (width * 3 + 1) + w * 3 + 1;
+            if(w == 0 || h == 0) {
+              p.r = data[base];
+              p.g = data[base + 1];
+              p.b = data[base + 2];
+            } else {
+              Pixel b1 = pixels[i - 1];
+              Pixel b2 = pixels[i - width];
+
+              p.r = add((static_cast<int>(b1.r) + b2.r) / 2, data[base]);
+              p.g = add((static_cast<int>(b1.g) + b2.g) / 2, data[base + 1]);
+              p.b = add((static_cast<int>(b1.b) + b2.b) / 2, data[base + 2]);
+            }
+            break;
+          }
+          case 4:
+          {
+            int base = h * (width * 3 + 1) + w * 3 + 1;
+            if(w == 0 || h == 0) {
+              p.r = data[base];
+              p.g = data[base + 1];
+              p.b = data[base + 2];
+            } else {
+              Pixel left = pixels[i - 1];
+              Pixel up = pixels[i - width];
+              Pixel naname = pixels[i - width - 1];
+              auto paethPredictor = [](Byte a, Byte b, Byte c) {
+                int pp = static_cast<int>(a) + b - c;
+                int pa = std::abs(pp - a);
+                int pb = std::abs(pp - b);
+                int pc = std::abs(pp - c);
+
+                if (pa <= pb && pa <= pc) {
+                  return a;
+                } else if (pb <= pc) {
+                  return b;
+                } else {
+                  return c;
+                }
+              };
+
+              p.r = add(paethPredictor(left.r, up.r, naname.r), data[base]);
+              p.g = add(paethPredictor(left.g, up.g, naname.g), data[base + 1]);
+              p.b = add(paethPredictor(left.b, up.b, naname.b), data[base + 2]);
+            }
+            break;
+          }
+          }
+        }
+        pixels[i] = p;
+        }
+      }
+    }
+    return pixels;
   }
 
   std::unique_ptr<Image> load(std::istream& fs) {
@@ -197,8 +337,25 @@ namespace PNG {
     }
 
     std::vector<std::unique_ptr<Chunk>> chunks = readChunks(fs);
-    std::unique_ptr<IHDRChunk> ihdr = dynamic_unique_cast<IHDRChunk>(std::move(chunks[0]));
-    PNG* png = new PNG{ihdr->width(), ihdr->height()};
+    auto ihdr = dynamic_unique_cast<IHDRChunk>(std::move(chunks[0]));
+    size_t width = ihdr->width();
+    size_t height = ihdr->height();
+   std::cerr
+      << ihdr->depth() << ' '
+      << ihdr->colorType() << ' '
+      << ihdr->compression() << ' '
+      << ihdr->filter() << ' '
+      << ihdr->interlace() << std::endl;
+    chunks[0] = std::make_unique<Chunk>("IHDRn");
+    std::vector<Byte> data = concatIDAT(chunks);
+    data = Deflate::decompress(data);
+// //    std::cout << "size: " << data.size() << std::endl;
+    // for(auto e: data) {
+    //   std::cout << int(e) << ' ';
+    // }
+    // std::cout << std::endl;
+    std::vector<Pixel> pixels = render(std::move(ihdr), data);
+    PNG* png = new PNG{width, height, pixels};
     return std::unique_ptr<Image>{png};
   }
 }
