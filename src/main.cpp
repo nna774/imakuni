589ee2c4a2e25711d13db39476ee94944cd0c2c4
Kugayama Nana <nonamea774@gmail.com>
2018-07-29 03:08:15 +0900

    omit as pnm

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,20 @@
 
 #include "png.h"
 
+namespace PNM {
+  std::unique_ptr<Image> exportPNM(std::unique_ptr<Image>&& img, std::ostream& os) {
+    size_t width = img->width();
+    size_t height = img->height();
+    std::vector<Pixel> const& ps = img->pixels();
+    os << "P3" << std::endl;
+    os << width << ' ' << height << std::endl;
+    os << 255 << std::endl;
+    for(auto e: ps) {
+      os << static_cast<int>(e.r) << ' ' << static_cast<int>(e.g) << ' ' << static_cast<int>(e.b) << std::endl;
+    }
+  }
+}
+
 int main(int argc, char** argv) {
   std::unique_ptr<Image> img;
   if(argc >= 2) {
@@ -16,6 +30,6 @@ int main(int argc, char** argv) {
     img = PNG::load(std::cin);
   }
 
-  std::cout << img->width() << ' ' << img->height() << std::endl;
+  PNM::exportPNM(std::move(img), std::cout);
   return 0;
 }
