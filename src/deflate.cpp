5602bb80dadb3ae8b94cbd444b7fef601a9b7979
Kugayama Nana <nonamea774@gmail.com>
2018-08-07 21:24:51 +0900

    impl Deflate::compress

--- src/deflate.cpp
+++ src/deflate.cpp
@@ -1,19 +1,35 @@
+#include <algorithm>
+#include <memory>
+
 #include "deflate.h"
 
+#define MINIZ_NO_ZLIB_COMPATIBLE_NAMES
 #include "miniz.c"
 
+using std::begin;
+
 namespace Deflate {
+  std::vector<Byte> compress(std::vector<Byte> const& src) {
+    unsigned long size = mz_compressBound(src.size());
+    std::unique_ptr<Byte> buf{new Byte[size]};
+    int status = mz_compress(buf.get(), &size, src.data(), src.size());
+    if (status != MZ_OK) {
+      return {};
+    }
+    std::vector<Byte> v(size);
+    std::copy_n(buf.get(), size, begin(v));
+    return v;
+  }
+
   std::vector<Byte> decompress(std::vector<Byte> const& src) {
-    std::vector<Byte> v;
     unsigned long size = src.size() * 100; //
-    Byte *buf = new Byte[size];
-    int cmp_status = uncompress(buf, &size, src.data(), src.size());
-    if (cmp_status != Z_OK) {
-      return v;
+    std::unique_ptr<Byte> buf{new Byte[size]};
+    int status = mz_uncompress(buf.get(), &size, src.data(), src.size());
+    if (status != MZ_OK) {
+      return {};
     }
-    for(unsigned long i{0}; i < size; ++i) {
-      v.push_back(buf[i]);
-    }
+    std::vector<Byte> v(size);
+    std::copy_n(buf.get(), size, begin(v));
     return v;
   }
 }
--- src/deflate.h
+++ src/deflate.h
@@ -5,5 +5,6 @@
 #pragma once
 
 namespace Deflate {
+  std::vector<Byte> compress(std::vector<Byte> const& src);
   std::vector<Byte> decompress(std::vector<Byte> const& src);
 }
