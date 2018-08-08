62059b399d06b683dbc5d6f64b6fe8b04e4152e9
Kugayama Nana <nonamea774@gmail.com>
2018-08-08 21:33:45 +0900

    実行時の引数を変更

--- src/main.cpp
+++ src/main.cpp
@@ -1,22 +1,79 @@
 #include <iostream>
+#include <array>
+#include <string>
 #include <fstream>
+#include <functional>
 
 #include "png.h"
 #include "pnm.h"
 
+template<class T, class... Args>
+inline std::array<T, sizeof...(Args)> make_array(Args &&... args) {
+  return std::array< T, sizeof...(Args) >{ std::forward<Args>(args)... };
+}
+
+using loadType = std::function<std::unique_ptr<Image>(std::istream&)>;
+using exportType = std::function<std::unique_ptr<Image>(std::unique_ptr<Image>&&, std::ostream&)>;
+auto availableExts = make_array<std::tuple<std::string, loadType, exportType>>(
+  std::make_tuple("png", PNG::load, PNG::exportPNG),
+  std::make_tuple("pnm", PNM::load, PNM::exportPNM)
+);
+
+auto hasSuffix = [](std::string const& str, std::string const& suffix) {
+  return str.size() >= suffix.size() && str.find(suffix, str.size() - suffix.size()) != std::string::npos;
+};
+
 int main(int argc, char** argv) {
   std::unique_ptr<Image> img;
-  if(argc >= 2) {
-    std::ifstream fs{argv[1], std::ifstream::binary};
-    if (!fs.is_open()) {
-      std::cerr << "failed to open" << std::endl;
-      return -1;
-    }
-    img = PNG::load(fs);
-  } else {
-    img = PNG::load(std::cin);
+  if(argc < 3) {
+    std::cerr << argv[0] << " infile outfile" << std::endl;
+    return -1;
   }
 
-  PNG::exportPNG(std::move(img), std::cout);
+  std::string in{argv[1]}, out{argv[2]};
+  bool okIn{false}, okOut{false};
+  for(auto e: availableExts) {
+    auto ext = std::get<0>(e);
+    auto load = std::get<1>(e);
+    if(in == ext + ":-") {
+      img = load(std::cin);
+    } else if (hasSuffix(in, "." + ext)) {
+      std::ifstream fs{in, std::ifstream::binary};
+      if (!fs.is_open()) {
+        std::cerr << "failed to open " << in << std::endl;
+        return -1;
+      }
+      img = load(fs);
+    }
+    if(img) {
+      okIn = true;
+      break;
+    }
+  }
+
+  if(!okIn) {
+    std::cerr << "input file " << in << " is not supported." << std::endl;
+  }
+
+  for(auto e: availableExts) {
+    auto ext = std::get<0>(e);
+    auto export_ = std::get<2>(e);
+    if(out == ext + ":-") {
+      export_(std::move(img), std::cout);
+      okOut = true;
+    } else if (hasSuffix(out, "." + ext)) {
+      std::ofstream fs{out, std::ofstream::binary};
+      if (!fs.is_open()) {
+        std::cerr << "failed to open " << out << std::endl;
+        return -1;
+      }
+      export_(std::move(img), fs);
+      okOut = true;
+    }
+  }
+  if(!okOut) {
+    std::cerr << "output file " << out << " is not supported." << std::endl;
+  }
+
   return 0;
 }
