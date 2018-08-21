416ff87f8b57f9eb4deb5fa23ca445a564327b1a
Kugayama Nana <nonamea774@gmail.com>
2018-08-21 17:50:01 +0900

    to_strをByteでも使えるようにする。

--- src/to_string.h
+++ src/to_string.h
@@ -12,3 +12,9 @@ inline std::string to_str(std::array<Byte, N> const& arr) {
   }
   return buf.str();
 }
+
+inline std::string to_str(Byte b) {
+  std::stringstream buf;
+  buf << std::hex << static_cast<int>(b);
+  return buf.str();
+}
