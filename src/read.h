ff9ac688b0a16ffc8e0a99d01727a484ed551069
Kugayama Nana <nonamea774@gmail.com>
2018-08-22 14:10:39 +0900

    TとしてByte以外の型が渡されるとおかしくなるのを修正。

--- src/read.h
+++ src/read.h
@@ -10,13 +10,13 @@ template<typename T>
 inline void read(std::istream& fs, T& p) {
   fs.read(reinterpret_cast<char*>(&p), sizeof(T));
 }
-template<>
-inline void read(std::istream& fs, std::vector<Byte>& arr) {
-  fs.read(reinterpret_cast<char*>(arr.data()), arr.size());
+template<typename T>
+inline void read(std::istream& fs, std::vector<T>& arr) {
+  fs.read(reinterpret_cast<char*>(arr.data()), arr.size() * sizeof(T));
 }
-template<size_t N>
-inline void read(std::istream& fs, std::array<Byte, N>& arr) {
-  fs.read(reinterpret_cast<char*>(arr.data()), N);
+template<typename T, size_t N>
+inline void read(std::istream& fs, std::array<T, N>& arr) {
+  fs.read(reinterpret_cast<char*>(begin(arr)), N * sizeof(T));
 }
 
 template<typename T>
@@ -26,13 +26,13 @@ inline void read(std::vector<Byte>::iterator& v, T& p) {
     pp[i] = *(v++);
   }
 }
-template<>
-inline void read(std::vector<Byte>::iterator& v, std::vector<Byte>& arr) {
-  std::copy_n(v, arr.size(), begin(arr));
-  std::advance(v, arr.size());
+template<typename T>
+inline void read(std::vector<Byte>::iterator& v, std::vector<T>& arr) {
+  std::copy_n(v, arr.size() * sizeof(T), begin(arr));
+  std::advance(v, arr.size() * sizeof(T));
 }
 template<typename T, size_t N>
 inline void read(std::vector<Byte>::iterator& v, std::array<T, N>& p) {
-  std::copy_n(v, N, reinterpret_cast<char*>(begin(p)));
-  std::advance(v, N);
+  std::copy_n(v, N * sizeof(T), reinterpret_cast<char*>(begin(p)));
+  std::advance(v, N * sizeof(T));
 }
